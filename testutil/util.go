bd6117d6a45321aaa4792a753be8e62a18800c79
Mathias Fredriksson <mafredri@gmail.com>
2024-08-20 09:44:15 +0000

    Add t.Helper entries

--- testutil/util.go
+++ testutil/util.go
@@ -48,6 +48,7 @@ type CurrentUser struct {
 }
 
 func GetCurrentUser(t *testing.T) CurrentUser {
+	t.Helper()
 	currentUser, err := user.Current()
 	if err != nil {
 		t.Fatalf("Cannot get current user: %s", err)
@@ -90,6 +91,7 @@ func CheckErrorAndDeepEqual(t *testing.T, shouldErr bool, err error, expected, a
 }
 
 func CheckError(t *testing.T, shouldErr bool, err error) {
+	t.Helper()
 	if err := checkErr(shouldErr, err); err != nil {
 		t.Error(err)
 	}
