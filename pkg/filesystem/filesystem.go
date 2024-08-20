04657852b612de00e4e0aebda6a6e3f9d1ce14f6
Mathias Fredriksson <mafredri@gmail.com>
2024-08-20 09:25:10 +0000

    Fix?

--- pkg/filesystem/filesystem.go
+++ pkg/filesystem/filesystem.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-	http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -27,12 +27,15 @@ import (
 // FS is the default filesystem used by the package.
 var FS vfs.FS = vfs.OSFS
 
-func SetFS(fs vfs.FS) {
-	FS = fs
-}
-
+// IOFS returns the default filesystem as an io/fs.FS.
 func IOFS() iofs.FS {
 	return FS
+}
+
+// Set the filesystem to be used by the package. Changing the filesystem
+// is not safe after the package has been used.
+func SetFS(fs vfs.FS) {
+	FS = fs
 }
 
 // TODO(mafredri): Replace with non-os specific functions.
